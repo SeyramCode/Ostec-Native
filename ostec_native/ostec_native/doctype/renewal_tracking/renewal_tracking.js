2e3c29fb8b8c7e8b6c624d063f74bd3693bf8261
SeyramCode <rgedzia@gmail.com>
2026-01-26 14:16:10 +0000

    Added Renewal Tracking doctype

---
+++ ostec_native/ostec_native/doctype/renewal_tracking/renewal_tracking.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Richmond Gedziq and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Renewal Tracking", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ ostec_native/ostec_native/doctype/renewal_tracking/test_renewal_tracking.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Richmond Gedziq and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRenewalTracking(FrappeTestCase):
+	pass
