a4968a7e9e029165a9bc838947d7a8499db3c2f5
SeyramCode <rgedzia@gmail.com>
2026-01-27 10:12:04 +0000

    Renewal doctype completed

--- ostec_native/ostec_native/doctype/renewal_tracking/renewal_tracking.js
+++ ostec_native/ostec_native/doctype/renewal_tracking/renewal_tracking.js
@@ -1,8 +1,403 @@
 // Copyright (c) 2026, Richmond Gedziq and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Renewal Tracking", {
-// 	refresh(frm) {
+frappe.ui.form.on('Renewal Tracking', {
+    refresh: function(frm) {
+        // Recalculate items on refresh to ensure values are correct
+        if (frm.doc.items && frm.doc.items.length > 0) {
+            frm.doc.items.forEach(function(item) {
+                calculate_item_values(frm, item.doctype, item.name);
+            });
+        }
+        
+        // Add Upload/Download buttons for items table with delay to ensure grid is rendered
+        setTimeout(function() {
+            add_upload_download_buttons(frm);
+        }, 500);
+        
+        // Add custom buttons to create documents
+        if (frm.doc.docstatus === 1) {
+            // Request for Quotation button
+            frm.add_custom_button(__('Request for Quotation'), function() {
+                frappe.model.open_mapped_doc({
+                    method: "ostec_native.ostec_native.doctype.renewal_tracking.renewal_tracking.make_request_for_quotation",
+                    frm: frm
+                });
+            }, __('Create'));
+            
+            // Supplier Quotation button
+            frm.add_custom_button(__('Supplier Quotation'), function() {
+                frappe.model.open_mapped_doc({
+                    method: "ostec_native.ostec_native.doctype.renewal_tracking.renewal_tracking.make_supplier_quotation",
+                    frm: frm
+                });
+            }, __('Create'));
+            
+            // Customer Quotation button
+            frm.add_custom_button(__('Quotation'), function() {
+                frappe.model.open_mapped_doc({
+                    method: "ostec_native.ostec_native.doctype.renewal_tracking.renewal_tracking.make_quotation",
+                    frm: frm
+                });
+            }, __('Create'));
+        }
+    },
+    
+    company: function(frm) {
+        // When company changes, reset and recalculate
+        if (frm.doc.currency) {
+            get_exchange_rate(frm);
+        }
+    },
+    
+    currency: function(frm) {
+        // Fetch exchange rate when currency changes
+        get_exchange_rate(frm);
+    },
+    
+    exchange_rate: function(frm) {
+        // Recalculate all items when exchange rate changes
+        if (frm.doc.items && frm.doc.items.length > 0) {
+            frm.doc.items.forEach(function(item) {
+                calculate_item_values(frm, item.doctype, item.name);
+            });
+        }
+        calculate_totals(frm);
+    }
+});
 
-// 	},
-// });
+frappe.ui.form.on('Renewal Tracking Item', {
+    qty: function(frm, cdt, cdn) {
+        calculate_item_values(frm, cdt, cdn);
+        calculate_totals(frm);
+    },
+    
+    rate: function(frm, cdt, cdn) {
+        calculate_item_values(frm, cdt, cdn);
+        calculate_totals(frm);
+    },
+    
+    items_add: function(frm, cdt, cdn) {
+        calculate_item_values(frm, cdt, cdn);
+        calculate_totals(frm);
+        // Re-add buttons after adding new row
+        setTimeout(function() {
+            add_upload_download_buttons(frm);
+        }, 300);
+    },
+    
+    items_remove: function(frm) {
+        calculate_totals(frm);
+    }
+});
+
+function add_upload_download_buttons(frm) {
+    // Get the items grid
+    let grid = frm.fields_dict.items.grid;
+    
+    if (!grid || !grid.wrapper) return;
+    
+    // Find or create the grid buttons area
+    let grid_buttons_area = grid.wrapper.find('.grid-buttons');
+    
+    if (!grid_buttons_area.length) {
+        // If grid-buttons doesn't exist, create it
+        let grid_footer = grid.wrapper.find('.grid-footer');
+        if (grid_footer.length) {
+            grid_footer.prepend('<div class="grid-buttons"></div>');
+            grid_buttons_area = grid.wrapper.find('.grid-buttons');
+        } else {
+            return;
+        }
+    }
+    
+    // Remove existing custom buttons to prevent duplicates
+    grid_buttons_area.find('.custom-upload-download-buttons').remove();
+    
+    // Create button container with right alignment
+    let button_html = `
+        <div class="custom-upload-download-buttons" style="float: right; margin-right: 10px;">
+            <button class="btn btn-xs btn-default btn-download-items" type="button" style="margin-left: 5px;">
+                <svg class="icon icon-sm" style=""><use href="#icon-download"></use></svg>
+                ${__('Download')}
+            </button>
+    `;
+    
+    // Add Upload button only if document is not submitted
+    if (frm.doc.docstatus === 0) {
+        button_html += `
+            <button class="btn btn-xs btn-default btn-upload-items" type="button" style="margin-left: 5px;">
+                <svg class="icon icon-sm" style=""><use href="#icon-upload"></use></svg>
+                ${__('Upload')}
+            </button>
+        `;
+    }
+    
+    button_html += '</div>';
+    
+    grid_buttons_area.prepend(button_html);
+    
+    // Attach click handlers
+    grid_buttons_area.find('.btn-download-items').off('click').on('click', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        download_items_template(frm);
+        return false;
+    });
+    
+    grid_buttons_area.find('.btn-upload-items').off('click').on('click', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        upload_items_from_file(frm);
+        return false;
+    });
+    
+    // Prevent Enter key from triggering download
+    grid_buttons_area.find('.btn-download-items, .btn-upload-items').on('keydown', function(e) {
+        if (e.keyCode === 13) { // Enter key
+            e.preventDefault();
+            e.stopPropagation();
+            $(this).click();
+            return false;
+        }
+    });
+}
+
+function download_items_template(frm) {
+    // Prepare data for download
+    let data = [];
+    
+    // Add headers
+    data.push([
+        'Item Code',
+        'Item Name',
+        'Description',
+        'Brand',
+        'Item Group',
+        'UOM',
+        'Qty',
+        'Rate',
+        'Amount',
+        'Base Rate',
+        'Base Amount'
+    ]);
+    
+    // Add existing items data
+    if (frm.doc.items && frm.doc.items.length > 0) {
+        frm.doc.items.forEach(function(item) {
+            data.push([
+                item.item_code || '',
+                item.item_name || '',
+                item.description ? strip_html(item.description) : '',
+                item.brand || '',
+                item.item_group || '',
+                item.oum || '',
+                item.qty || 0,
+                item.rate || 0,
+                item.amount || 0,
+                item.base_rate || 0,
+                item.base_amount || 0
+            ]);
+        });
+    } else {
+        // Add empty rows as template
+        for (let i = 0; i < 5; i++) {
+            data.push(['', '', '', '', '', '', '', '', '', '', '']);
+        }
+    }
+    
+    // Create CSV content
+    let csv_content = data.map(row => row.map(cell => {
+        // Escape double quotes and wrap in quotes if contains comma
+        let cell_str = String(cell);
+        if (cell_str.includes(',') || cell_str.includes('"') || cell_str.includes('\n')) {
+            return '"' + cell_str.replace(/"/g, '""') + '"';
+        }
+        return cell_str;
+    }).join(',')).join('\n');
+    
+    // Download file
+    let blob = new Blob([csv_content], { type: 'text/csv;charset=utf-8;' });
+    let link = document.createElement('a');
+    let url = URL.createObjectURL(blob);
+    link.setAttribute('href', url);
+    link.setAttribute('download', 'renewal_tracking_items_' + (frm.doc.name || 'template') + '.csv');
+    link.style.visibility = 'hidden';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    
+    frappe.show_alert({
+        message: __('Items downloaded successfully'),
+        indicator: 'green'
+    });
+}
+
+function upload_items_from_file(frm) {
+    // Check if document is submitted
+    if (frm.doc.docstatus === 1) {
+        frappe.msgprint(__('Cannot upload items to a submitted document'));
+        return;
+    }
+    
+    // Create file upload dialog
+    new frappe.ui.FileUploader({
+        doctype: frm.doctype,
+        docname: frm.docname,
+        frm: frm,
+        folder: 'Home',
+        restrictions: {
+            allowed_file_types: ['.csv', '.xlsx', '.xls']
+        },
+        on_success: function(file_doc) {
+            // Process the uploaded file
+            frappe.call({
+                method: 'ostec_native.ostec_native.doctype.renewal_tracking.renewal_tracking.import_items',
+                args: {
+                    file_url: file_doc.file_url,
+                    parent_doc: frm.doc.name
+                },
+                callback: function(r) {
+                    if (r.message) {
+                        // Clear existing items
+                        frm.clear_table('items');
+                        
+                        // Add imported items
+                        r.message.forEach(function(item_data) {
+                            let item = frm.add_child('items');
+                            Object.assign(item, item_data);
+                        });
+                        
+                        // Refresh and recalculate
+                        frm.refresh_field('items');
+                        
+                        // Recalculate all values
+                        frm.doc.items.forEach(function(item) {
+                            calculate_item_values(frm, item.doctype, item.name);
+                        });
+                        calculate_totals(frm);
+                        
+                        frappe.show_alert({
+                            message: __('Items imported successfully'),
+                            indicator: 'green'
+                        });
+                    }
+                }
+            });
+        }
+    });
+}
+
+function strip_html(html) {
+    if (!html) return '';
+    let tmp = document.createElement('DIV');
+    tmp.innerHTML = html;
+    return tmp.textContent || tmp.innerText || '';
+}
+
+function get_exchange_rate(frm) {
+    if (!frm.doc.currency || !frm.doc.company) {
+        return;
+    }
+    
+    // Get company currency
+    frappe.db.get_value('Company', frm.doc.company, 'default_currency', function(r) {
+        let company_currency = r.default_currency;
+        
+        // Check if base currency is selected (Ostec Ltd = GHS, Ostec SA = CFA)
+        if (frm.doc.currency === company_currency) {
+            // Base currency selected, exchange rate = 1
+            frm.set_value('exchange_rate', 1.0);
+            return;
+        }
+        
+        // Try to find exchange rate from Currency Exchange doctype
+        frappe.call({
+            method: 'frappe.client.get_value',
+            args: {
+                doctype: 'Currency Exchange',
+                filters: {
+                    from_currency: frm.doc.currency,
+                    to_currency: company_currency
+                },
+                fieldname: 'exchange_rate'
+            },
+            callback: function(response) {
+                if (response.message && response.message.exchange_rate) {
+                    // Found exchange rate in system, set it
+                    frm.set_value('exchange_rate', flt(response.message.exchange_rate));
+                } else {
+                    // Currency pair not available, prompt user to enter rate manually
+                    frappe.prompt({
+                        label: 'Exchange Rate',
+                        fieldname: 'rate',
+                        fieldtype: 'Float',
+                        description: `Currency pair ${frm.doc.currency} to ${company_currency} not found in system. Please enter the exchange rate manually.`,
+                        default: frm.doc.exchange_rate || 1.0,
+                        reqd: 1
+                    }, function(values) {
+                        frm.set_value('exchange_rate', flt(values.rate));
+                    }, 'Enter Exchange Rate', 'Set Rate');
+                }
+            }
+        });
+    });
+}
+
+function calculate_item_values(frm, cdt, cdn) {
+    let row = locals[cdt][cdn];
+    
+    if (!row) return;
+    
+    // Calculate amount = qty * rate
+    row.amount = flt(row.qty, 2) * flt(row.rate, 2);
+    
+    // Get exchange rate from parent document
+    let exchange_rate = flt(frm.doc.exchange_rate) || 1.0;
+    
+    // Get company currency to check if conversion is needed
+    if (!frm.doc.company) {
+        row.base_rate = row.rate;
+        row.base_amount = row.amount;
+        frm.refresh_field('items');
+        return;
+    }
+    
+    frappe.db.get_value('Company', frm.doc.company, 'default_currency', function(r) {
+        let company_currency = r.default_currency;
+        
+        // Check if currency conversion is needed
+        if (frm.doc.currency && company_currency && frm.doc.currency !== company_currency) {
+            // Different currencies - apply exchange rate
+            row.base_rate = flt(row.rate, 2) * exchange_rate;
+            row.base_amount = flt(row.amount, 2) * exchange_rate;
+        } else {
+            // Same currency (base currency) - no conversion needed
+            row.base_rate = row.rate;
+            row.base_amount = row.amount;
+        }
+        
+        // Refresh the child table field
+        frm.refresh_field('items');
+    });
+}
+
+function calculate_totals(frm) {
+    if (!frm.doc.items || frm.doc.items.length === 0) {
+        frm.set_value('net_total', 0.0);
+        frm.set_value('net_total_base', 0.0);
+        return;
+    }
+    
+    let net_total = 0.0;
+    let net_total_base = 0.0;
+    
+    // Sum all amounts and base amounts
+    frm.doc.items.forEach(function(item) {
+        net_total += flt(item.amount, 2);
+        net_total_base += flt(item.base_amount, 2);
+    });
+    
+    frm.set_value('net_total', net_total);
+    frm.set_value('net_total_base', net_total_base);
+}
--- ostec_native/ostec_native/doctype/renewal_tracking/renewal_tracking.py
+++ ostec_native/ostec_native/doctype/renewal_tracking/renewal_tracking.py
@@ -2,8 +2,266 @@
 # For license information, please see license.txt
 
 # import frappe
+# -*- coding: utf-8 -*-
+import json
+
+import frappe
 from frappe.model.document import Document
+from frappe.utils import flt,getdate,nowdate
+
 
 
 class RenewalTracking(Document):
-	pass
+    def validate(self):
+        self.set_exchange_rate()
+        self.calculate_item_values()
+        self.calculate_totals()
+    
+    def before_save(self):
+        self.calculate_item_values()
+        self.calculate_totals()
+    
+    def set_exchange_rate(self):
+        """Set exchange rate from Currency Exchange doctype"""
+        if not self.currency or not self.company:
+            return
+        
+        company_currency = frappe.db.get_value('Company', self.company, 'default_currency')
+        
+        # For Ostec Ltd (GHS) or Ostec SA (CFA) - if base currency selected, exchange rate = 1
+        if self.currency == company_currency:
+            self.exchange_rate = 1.0
+            return
+        
+        # If exchange rate already manually entered, keep it
+        if self.exchange_rate and self.exchange_rate > 0:
+            return
+        
+        # Try to find exchange rate from Currency Exchange doctype
+        exchange_rate_value = frappe.db.get_value('Currency Exchange',
+            {
+                'from_currency': self.currency,
+                'to_currency': company_currency
+            },
+            'exchange_rate'
+        )
+        
+        if exchange_rate_value:
+            # Found in system, store the exchange rate
+            self.exchange_rate = flt(exchange_rate_value)
+        else:
+            # Currency pair not available in system, user must enter manually
+            frappe.throw(
+                f'Currency pair {self.currency} to {company_currency} not available in system. '
+                'Please enter the exchange rate manually.'
+            )
+    
+    def calculate_item_values(self):
+        """Calculate amounts and base currency values for all items"""
+        if not self.items:
+            return
+        
+        # Get exchange rate
+        exchange_rate = flt(self.exchange_rate) or 1.0
+        company_currency = self.get_company_currency()
+        
+        for item in self.items:
+            # Calculate amount = qty * rate
+            item.amount = flt(item.qty, 2) * flt(item.rate, 2)
+            
+            # Calculate base currency values using exchange rate
+            if self.currency and company_currency and self.currency != company_currency:
+                # Different currencies - apply exchange rate
+                item.base_rate = flt(item.rate, 2) * exchange_rate
+                item.base_amount = flt(item.amount, 2) * exchange_rate
+            else:
+                # Same currency (base currency) - no conversion needed
+                item.base_rate = item.rate
+                item.base_amount = item.amount
+    
+    def calculate_totals(self):
+        """Calculate net_total and net_total_base from all item lines"""
+        if not self.items:
+            self.net_total = 0.0
+            self.net_total_base = 0.0
+            return
+        
+        # Sum all amounts
+        self.net_total = sum(flt(item.amount, 2) for item in self.items)
+        
+        # Sum all base amounts
+        self.net_total_base = sum(flt(item.base_amount, 2) for item in self.items)
+    
+    def get_company_currency(self):
+        """Get company's default currency"""
+        if self.company:
+            return frappe.db.get_value('Company', self.company, 'default_currency')
+        return None
+
+
+@frappe.whitelist()
+def import_items(file_url, parent_doc):
+    """Import items from uploaded CSV/Excel file"""
+    import csv
+    import openpyxl
+    from frappe.utils.file_manager import get_file_path
+    
+    # Get the file path
+    file_path = get_file_path(file_url)
+    
+    items = []
+    
+    try:
+        # Determine file type and read accordingly
+        if file_url.endswith('.csv'):
+            # Read CSV file
+            with open(file_path, 'r', encoding='utf-8-sig') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    if row.get('Item Code'):  # Skip empty rows
+                        items.append({
+                            'item_code': row.get('Item Code', ''),
+                            'item_name': row.get('Item Name', ''),
+                            'description': row.get('Description', ''),
+                            'brand': row.get('Brand', ''),
+                            'item_group': row.get('Item Group', ''),
+                            'oum': row.get('UOM', ''),
+                            'qty': flt(row.get('Qty', 0)),
+                            'rate': flt(row.get('Rate', 0)),
+                        })
+        else:
+            # Read Excel file
+            workbook = openpyxl.load_workbook(file_path)
+            sheet = workbook.active
+            
+            # Get headers from first row
+            headers = [cell.value for cell in sheet[1]]
+            
+            # Read data rows
+            for row in sheet.iter_rows(min_row=2, values_only=True):
+                if row[0]:  # Skip empty rows (check if Item Code exists)
+                    item_data = dict(zip(headers, row))
+                    items.append({
+                        'item_code': item_data.get('Item Code', ''),
+                        'item_name': item_data.get('Item Name', ''),
+                        'description': item_data.get('Description', ''),
+                        'brand': item_data.get('Brand', ''),
+                        'item_group': item_data.get('Item Group', ''),
+                        'oum': item_data.get('UOM', ''),
+                        'qty': flt(item_data.get('Qty', 0)),
+                        'rate': flt(item_data.get('Rate', 0)),
+                    })
+    except Exception as e:
+        frappe.throw(f'Error reading file: {str(e)}')
+    
+    return items
+
+
+@frappe.whitelist()
+def make_request_for_quotation(source_name, target_doc=None):
+    """Create Request for Quotation from Renewal Tracking"""
+    from frappe.model.mapper import get_mapped_doc
+    
+    def set_missing_values(source, target):
+        target.transaction_date = frappe.utils.nowdate()
+        target.status = "Draft"
+    
+    def update_item(source, target, source_parent):
+        target.schedule_date = frappe.utils.add_days(frappe.utils.nowdate(), 7)
+    
+    doclist = get_mapped_doc("Renewal Tracking", source_name, {
+        "Renewal Tracking": {
+            "doctype": "Request for Quotation",
+            "field_map": {
+                "name": "renewal_tracking",
+                "company": "company"
+            }
+        },
+        "Renewal Tracking Item": {
+            "doctype": "Request for Quotation Item",
+            "field_map": {
+                "item_code": "item_code",
+                "item_name": "item_name",
+                "description": "description",
+                "qty": "qty",
+                "oum": "uom",
+                "brand": "brand"
+            },
+            "postprocess": update_item
+        }
+    }, target_doc, set_missing_values)
+    
+    return doclist
+
+
+@frappe.whitelist()
+def make_supplier_quotation(source_name, target_doc=None):
+    """Create Supplier Quotation from Renewal Tracking"""
+    from frappe.model.mapper import get_mapped_doc
+    
+    def set_missing_values(source, target):
+        target.transaction_date = frappe.utils.nowdate()
+    
+    doclist = get_mapped_doc("Renewal Tracking", source_name, {
+        "Renewal Tracking": {
+            "doctype": "Supplier Quotation",
+            "field_map": {
+                "name": "renewal_tracking",
+                "company": "company",
+                "currency": "currency",
+                "exchange_rate": "conversion_rate"
+            }
+        },
+        "Renewal Tracking Item": {
+            "doctype": "Supplier Quotation Item",
+            "field_map": {
+                "item_code": "item_code",
+                "item_name": "item_name",
+                "description": "description",
+                "qty": "qty",
+                "oum": "uom",
+                "rate": "rate",
+                "amount": "amount",
+                "brand": "brand"
+            }
+        }
+    }, target_doc, set_missing_values)
+    
+    return doclist
+
+
+@frappe.whitelist()
+def make_quotation(source_name, target_doc=None):
+    """Create Customer Quotation from Renewal Tracking"""
+    from frappe.model.mapper import get_mapped_doc
+    
+    def set_missing_values(source, target):
+        target.transaction_date = frappe.utils.nowdate()
+        target.valid_till = frappe.utils.add_days(frappe.utils.nowdate(), 30)
+    
+    doclist = get_mapped_doc("Renewal Tracking", source_name, {
+        "Renewal Tracking": {
+            "doctype": "Quotation",
+            "field_map": {
+                "name": "renewal_tracking",
+                "company": "company",
+                "currency": "currency",
+                "exchange_rate": "conversion_rate"
+            }
+        },
+        "Renewal Tracking Item": {
+            "doctype": "Quotation Item",
+            "field_map": {
+                "item_code": "item_code",
+                "item_name": "item_name",
+                "description": "description",
+                "qty": "qty",
+                "oum": "uom",
+                "rate": "rate",
+                "amount": "amount",
+                "brand": "brand"
+            }
+        }
+    }, target_doc, set_missing_values)
+    
+    return doclist
